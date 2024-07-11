6bcdc74eff2a4e8cd1a4480bb9f518c55b34b6cc
KonTy <KonTy@github.com>
2024-07-11 11:14:57 -0700

    trying to get dwmblocks going

--- configs/scripts/set-background.sh
+++ configs/scripts/set-background.sh
@@ -12,5 +12,16 @@ image_index=$((tag + 1))
 # Construct the path to the background image
 background_image="$BACKGROUND_DIR/bg${image_index}.jpg"
 
-# Set the background using feh
-feh --bg-scale "$background_image"
+# Function to set background with a fade effect
+set_background_with_fade() {
+    # Set the background with feh initially (without transition)
+    feh --bg-scale "$background_image"
+
+    # Use picom to fade in the background image
+    picom-trans -c &
+    sleep 0.2  # Adjust this sleep duration for your desired fade-in speed
+    picom-trans -o
+}
+
+# Call the function to set background with fade effect
+set_background_with_fade
