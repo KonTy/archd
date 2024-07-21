8a35fdecd59b360be79383d43d80d2c9b686954d
KonTy <KonTy@github.com>
2024-07-20 18:41:51 -0700

    new changes

--- arch_post_install.sh
+++ arch_post_install.sh
@@ -85,7 +85,8 @@ declare -A prep_stage=(
     [ufw]="Uncomplicated Firewalll"
     [fail2ban]="ban clients that try to connect several times"
     [audit]="Audit nextwork and system"
-    [suricate]="Comprehensive network intrusion detection tool"
+    [rust]="rust libs"
+    [suricata]="Comprehensive network intrusion detection tool"
     [ossec-hids-local]="Virus/Intrusion detection tool"
     [fzf-git]="Fuzzy finder for terminal"
 )
